dddf29e26e5fc880f25901e2b0939ce58043a71b
Murilo <murilo.tonassi@alunoescolassociais.org.br>
2024-04-05 07:13:57 -0300

    modificação nos arquivos HTML e JavaScript

--- calendar.js
+++ calendar.js
@@ -1,8 +1,16 @@
-function colorirDia(){
-    let days     = document.getElementById('day').value;
-    let color    = document.getElementById('color').value;
-    let calendar = document.getElementById('calendar')
-    let td       = calendar.getElementsByTagName('td')[(parseInt(days)+2)];
-
-    td.style.backgroundColor = color;
+function validarDia() {
+    let days = document.getElementById('day').value;
+    if (days < 1 || days > 31) { // Verifica se o dia está dentro do intervalo desejado
+        alert("Por favor, digite um número entre 1 e 31.");
+    } else {
+        colorirDia(); // Se o dia estiver dentro do intervalo, chama a função para colorir o dia
+    }
+}
+ 
+function colorirDia() {
+    let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendarTable'); // Alterei o ID aqui para corresponder ao ID do primeiro elemento <table>
+    let td = calendar.getElementsByTagName('td')[(parseInt(days)-1)];
+td.style.backgroundColor = color;
 }
