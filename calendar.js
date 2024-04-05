ff49b34944bbb3553a3be9cf83b39adb1290bb07
Murilo <murilo.tonassi@alunoescolassociais.org.br>
2024-04-05 08:01:56 -0300

    implementação de uma nova função no JavaScript

--- calendar.js
+++ calendar.js
@@ -1,16 +1,13 @@
-function validarDia() {
-    let days = document.getElementById('day').value;
-    if (days < 1 || days > 31) { // Verifica se o dia está dentro do intervalo desejado
-        alert("Por favor, digite um número entre 1 e 31.");
-    } else {
-        colorirDia(); // Se o dia estiver dentro do intervalo, chama a função para colorir o dia
-    }
-}
- 
 function colorirDia() {
     let days = document.getElementById('day').value;
     let color = document.getElementById('color').value;
-    let calendar = document.getElementById('calendarTable'); // Alterei o ID aqui para corresponder ao ID do primeiro elemento <table>
-    let td = calendar.getElementsByTagName('td')[(parseInt(days)-1)];
-td.style.backgroundColor = color;
+    let calendar = document.getElementById('calendar');
+    let td = calendar.getElementsByTagName('td')[(parseInt(days) - 1)];
+
+    if (days < 1 || days > 30) {
+        alert("Por favor, digite um número de dia válido entre 01 e 30.");
+        return;
+    }
+
+    td.style.backgroundColor = color;
 }
